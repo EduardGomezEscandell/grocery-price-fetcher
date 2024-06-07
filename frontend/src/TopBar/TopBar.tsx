45629c299e336fbd1f13b321109a120168ef7d2a
Edu Gómez Escandell <eduard.gomez.escandell@gmail.com>
2024-06-07 18:33:36 +0200

    Allow interactive top bar title

--- frontend/src/TopBar/TopBar.tsx
+++ frontend/src/TopBar/TopBar.tsx
@@ -5,6 +5,8 @@ interface Props {
     left: JSX.Element
     right: JSX.Element
     logoOnClick?: () => void
+    titleOnClick?: () => void
+    titleText?: string|any
 }
 
 export default function TopBar(pp: Props): JSX.Element {
@@ -13,17 +15,17 @@ export default function TopBar(pp: Props): JSX.Element {
         display: 'flex',
     }
 
-    if (pp.logoOnClick == null) {
-        pp.logoOnClick = () => {}
-    }
-
     return (
         <div className='TopBar'>
             <div style={{...style, justifyContent: 'flex-start'}}>
                 {pp.left}
             </div>
-            <div style={{...style, justifyContent: 'center'}} onClick={pp.logoOnClick}>
-                <Title />
+            <div style={{...style, justifyContent: 'center'}}>
+                <Title
+                    logoOnClick={pp.logoOnClick || (() => {})}
+                    titleOnClick={pp.titleOnClick || (() => {})}
+                    titleText={pp.titleText}
+                    />
             </div>
             <div style={{...style, justifyContent: 'flex-end'}}>
                 {pp.right}
@@ -32,11 +34,17 @@ export default function TopBar(pp: Props): JSX.Element {
     )
 }
 
-function Title(): JSX.Element {
+interface TitleProps {
+    logoOnClick?: () => void
+    titleOnClick?: () => void
+    titleText?: string|null
+}
+
+function Title(pp: TitleProps): JSX.Element {
     return <div key='1' className='Title'>
-        <img src='/logo64.png' alt='logo' className="Logo"/>
-        <div className='Text'>
-            La&nbsp;compra de&nbsp;l'Edu
+        <img src='/logo64.png' alt='logo' className="Logo" onClick={pp.logoOnClick}/>
+        <div className='Text' onClick={pp.titleOnClick}>
+            {pp.titleText || "La\xa0compra de\xa0l'Edu" /* \xa0 is a non-breaking space */} 
         </div>
     </div>
 }
