a9b3ded9766720a5cb2bc852ad0129e1498c2d48
Edu Gómez Escandell <eduard.gomez.escandell@gmail.com>
2024-06-13 21:07:16 +0200

    Create 404 front-end page

--- frontend/src/Screens/Root.tsx
+++ frontend/src/Screens/Root.tsx
@@ -5,6 +5,7 @@ import RenderMenu from "./Menu/Menu";
 import RenderPantry from "./Pantry/Pantry";
 import ShoppingList from './ShoppingList/ShoppingList';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound from './404/NotFound.tsx/NotFound';
 
 export default function Root(): JSX.Element {
     const backend = new Backend();
@@ -17,6 +18,7 @@ export default function Root(): JSX.Element {
                 <Route path="/menu" element={<RenderMenu backend={backend} sessionName={sessionName} />} />
                 <Route path="/pantry" element={<RenderPantry backend={backend} sessionName={sessionName} />} />
                 <Route path="/shopping-list" element={<ShoppingList backend={backend} sessionName={sessionName} />} />
+                <Route path="*" element={<NotFound/>} />
             </Routes>
         </BrowserRouter>
     );
