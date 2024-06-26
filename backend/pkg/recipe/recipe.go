8c5a5961145d0cd007e854735bd55518298d13f3
Edu Gómez Escandell <eduard.gomez.escandell@gmail.com>
2024-06-26 22:02:25 +0200

    Give recipes an ID

--- backend/pkg/recipe/recipe.go
+++ backend/pkg/recipe/recipe.go
@@ -2,9 +2,11 @@ package recipe
 
 import "github.com/EduardGomezEscandell/grocery-price-fetcher/backend/pkg/product"
 
+type ID uint32
 
 // Recipe represents a combination of ingredients that can be used to prepare a dish.
 type Recipe struct {
+	ID          ID
 	Name        string
 	Ingredients []Ingredient
 }
