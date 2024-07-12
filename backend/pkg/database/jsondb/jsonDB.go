b12a78e841034db79da3209c8cdf88b640748a79
Edu Gómez Escandell <eduard.gomez.escandell@gmail.com>
2024-07-12 13:33:58 +0200

    Make JSON DB save file deterministically ordered
    This avoids flaky tests

--- backend/pkg/database/jsondb/jsonDB.go
+++ backend/pkg/database/jsondb/jsonDB.go
@@ -1,6 +1,7 @@
 package jsondb
 
 import (
+	"cmp"
 	"context"
 	"encoding/json"
 	"errors"
@@ -9,6 +10,7 @@ import (
 	"os"
 	"path/filepath"
 	"slices"
+	"strings"
 	"sync"
 	"time"
 
@@ -652,6 +654,16 @@ func load(path string, ptr interface{}) error {
 }
 
 func (db *JSON) save() error {
+	slices.SortFunc(db.users, strings.Compare)
+	slices.SortFunc(db.sessions, func(a, b dbtypes.Session) int { return cmp.Compare(a.ID, b.ID) })
+	slices.SortFunc(db.products, func(a, b product.Product) int { return cmp.Compare(a.ID, b.ID) })
+	slices.SortFunc(db.recipes, func(a, b recipe.Recipe) int { return cmp.Compare(a.ID, b.ID) })
+	slices.SortFunc(db.menus, func(a, b dbtypes.Menu) int { return multiCompare(c(a.User, b.User), c(a.Name, b.Name)) })
+	slices.SortFunc(db.pantries, func(a, b dbtypes.Pantry) int { return multiCompare(c(a.User, b.User), c(a.Name, b.Name)) })
+	slices.SortFunc(db.shoppingLists, func(a, b dbtypes.ShoppingList) int {
+		return multiCompare(c(a.User, b.User), c(a.Menu, b.Menu), c(a.Pantry, b.Pantry))
+	})
+
 	return errors.Join(
 		save(db.log, db.usersPath, db.users),
 		save(db.log, db.sesionsPath, db.sessions),
@@ -753,4 +765,22 @@ func partition[T any](slice []T, predicate func(T) bool) (p int) {
 		p++
 	}
 	return p
+}
+
+type comparison func() int
+
+func c[T cmp.Ordered](x, y T) comparison {
+	return func() int {
+		return cmp.Compare(x, y)
+	}
+}
+
+func multiCompare(c ...comparison) int {
+	for _, f := range c {
+		if c := f(); c != 0 {
+			return c
+		}
+	}
+
+	return 0
 }
